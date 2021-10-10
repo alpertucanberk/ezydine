99cedcc81dde93abf4267e06042ca1420fae4f48
Alper Canberk <alper.tu.canberk@gmail.com>
2021-10-10 08:43:39 -0400

    Change?

--- ezydine-frontend/pages/hall/[hall_id]/info.js
+++ ezydine-frontend/pages/hall/[hall_id]/info.js
@@ -192,6 +192,9 @@ export async function getServerSideProps() {
              </div>
          </div>
          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+<<<<<<< HEAD
+>>>>>>> parent of 8ddea08 (some stuff)
+=======
 >>>>>>> parent of 8ddea08 (some stuff)
           <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Menu Items</h2>
 
